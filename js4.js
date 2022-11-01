4d6904afe35a3a7a14e787a73f4cc206848b5da4
cyentific <112978621+cyentific@users.noreply.github.com>
2022-11-01 14:28:30 -0400

    Create js4.js

---
+++ js4.js
@@ -0,0 +1,17 @@
+function functionConditions(x) {
+    if (x == 'Wine') {
+        console.log(x + ' What is it, Tuesday?');
+    } else if (x == 'Tequila') {
+        console.log(x + ' Sounds like a good choice.');
+    } else if (x == 'Vodka') {
+        console.log(x + ' Grow up.');
+    } else if (x == 'Gin') {
+        console.log(x + ' I guess.');
+    } else if (x == 'Rum') {
+        console.log(x + ' Now you have taste!');
+    } else if (x == 'Whiskey') {
+        console.log(x + ' Say less.');
+    } else {
+        console.log('Sorry, but absolutely not');
+    }
+}
